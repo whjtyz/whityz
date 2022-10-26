8727af7831dbe92ac7d1713b38037705d5a3a69e
whityz <whjtyz@163.com>
2022-10-26 15:56:46 +0800

    配置eslint和prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 1824,
+    open: false
+  },
+  base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
